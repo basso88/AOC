d7214297a416e4c69cd1f9d198762cafcd3df46e
basso88 <marco.bassani88@gmail.com>
2026-01-01 23:51:14 +0100

    day 5.2 solved

--- 2025/day5/day5.2.py
+++ 2025/day5/day5.2.py
@@ -11,38 +11,38 @@ splitted_range_list = []
 
 for ran in sorted(range_ids):
     r = ran.split("-")
-    ran_list=[r[0],r[1]]
+    ran_list=[int(r[0]),int(r[1])]
     splitted_range_list.append(ran_list)
 
-merge = True
+splitted_range_list = sorted(splitted_range_list, key=lambda x : x[0])
+
+merge=True
 while merge:
+    merge=False
+    n = 0
     merged_ranges_list = []
-    merge = False
-    for n in range(len(splitted_range_list)):
-        if n+1 >= len(splitted_range_list):
-            merged_ranges_list.append(splitted_range_list[n])
-            continue
+    while n < len(splitted_range_list):
+        if n+1 < len(splitted_range_list):
             
-        if splitted_range_list[n][0]< splitted_range_list[n+1][0] and splitted_range_list[n][1] > splitted_range_list[n+1][0]:
-            new_range_list = [splitted_range_list[n][0], splitted_range_list[n+1][1]]
-            merged_ranges_list.append(new_range_list)
-            merge = True
-            break
-            
-            
-        elif splitted_range_list[n][1] < splitted_range_list[n+1][0]:
-            merged_ranges_list.append(splitted_range_list[n])
+            if splitted_range_list[n][1] < splitted_range_list[n+1][0]:
+                merged_ranges_list.append(splitted_range_list[n])
+                
 
+            elif splitted_range_list[n][1] >= splitted_range_list[n+1][0]:
+                merged_range = [splitted_range_list[n][0], max(splitted_range_list[n+1][1], splitted_range_list[n][1])] 
+                merged_ranges_list.append(merged_range)
+                merge = True
+                n+=1
+        else:
+            merged_ranges_list.append(splitted_range_list[n])
+                    
+        n+=1
+                
     splitted_range_list = merged_ranges_list
-        
-
-for n in range(len(merged_ranges_list)-1, 0, -1):
-    if merged_ranges_list[n][1] == merged_ranges_list[n-1][1]:
-        merged_ranges_list.pop(n-1)
-       
+    
 
 for ran in merged_ranges_list:
-    fresh_count += len(range(int(ran[0]),int(ran[1])))
+    fresh_count += len(range(ran[0],ran[1]+1))
 
-print(merged_ranges_list, "\n") 
+# print(merged_ranges_list, len(merged_ranges_list), "\n") 
 print(fresh_count)
--- 2025/day5/temp.py
+++ 2025/day5/temp.py
@@ -1,15 +1,49 @@
-lst = [x for x in range(1,11)]
 
+    
+    
+ranges = '''3-5
+10-14
+16-20
+17-18'''
+fresh_count = 0
+ranges_list = ranges.strip().split()
+splitted_range_list = []
 
-sum=True
-while sum:
-    sum=False
-    sum_list=lst
+for ran in ranges_list:
+    r = ran.split("-")
+    ran_list=[int(r[0]),int(r[1])]
+    splitted_range_list.append(ran_list)
     
-    sum_list[0]+sum_list[1]
-      
-    sum=True
+splitted_range_list = sorted(splitted_range_list, key=lambda x : x[0])
+
+merge=True
+while merge:
+    merge=False
+    n = 0
+    merged_ranges_list = []
+    while n < len(splitted_range_list):
+        if n+1 < len(splitted_range_list):
+            
+            if splitted_range_list[n][1] < splitted_range_list[n+1][0]:
+                merged_ranges_list.append(splitted_range_list[n])
+                
+
+            elif splitted_range_list[n][1] >= splitted_range_list[n+1][0]:
+                merged_range = [splitted_range_list[n][0], max(splitted_range_list[n+1][1],splitted_range_list[n][1])]
+                merged_ranges_list.append(merged_range)
+                merge = True
+                n += 1
+
+        else:
+            merged_ranges_list.append(splitted_range_list[n])       
+        n+=1
+                
+    splitted_range_list = merged_ranges_list
+
+print(merged_ranges_list)
+for r in merged_ranges_list:
+    fresh_count += len(range(r[0], r[1]+1))        
+
+print(fresh_count)
     
-    print(sum_list)
-    
-    
+    # print( splitted_range_list, len(splitted_range_list))
