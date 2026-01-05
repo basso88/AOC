b4e0ac1d2c5b7411d9f07aa49439040a48e27d1a
basso88 <marco.bassani88@gmail.com>
2026-01-05 22:21:31 +0100

    code clansing

--- 2025/day6/day6.2.py
+++ 2025/day6/day6.2.py
@@ -1,11 +1,10 @@
 
 import math
 
-inp = "/Users/marco/Developer/advent_of_code/2025/day6/input.txt"
-filehandl= open(inp)
+input = "/Users/marco/Developer/advent_of_code/2025/day6/input.txt"
+filehandl= open(input)
 problems_rows = filehandl.readlines()
 
-result = 0
 result_list = []
 nums_list = []
 operator = []
@@ -18,11 +17,8 @@ while i < len(problems_rows[0]):
         
         if row[i] == "+" or row[i] == "*":
             operator.append(row[i])
-        else:
-            continue
-
+        
     if num.isdigit():
-        num.strip()
         nums_list.append(int(num))
     else:
         if operator[0] =='*':
@@ -34,11 +30,11 @@ while i < len(problems_rows[0]):
             nums_list = []
             operator = []
 
-        
     i+=1
-            
-    
-nums_list = []   
+
 print(sum(result_list))
+        
+  
+    
     
 
